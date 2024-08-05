5b35e6a583acf34b1a627968c21b57174f1888fa
arunputhal <arunputhal@gmail.com>
2024-08-05 22:28:04 +0530

    comments in python

---
+++ usage_of_comments.py
@@ -0,0 +1,8 @@
+# This is a simple arithmatic caluclation
+# Author: Arun Kumar Puthal
+# Date: 5th August, 2024
+# Version: v1.0.0
+a = 4
+b = 5
+result = a + b
+print(f"The addition of {a} and {b} is: {result}")
