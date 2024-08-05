8f7011c0ec84ee4d386d927ba5bf296dd5657f43
arunputhal <arunputhal@gmail.com>
2024-08-05 23:57:23 +0530

    two new concepts

---
+++ working_with_datatypes.py
@@ -0,0 +1,35 @@
+# Number Datatypes
+x=3
+y=5.6
+z=3+4j
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(x, y, z)
+
+# String Datatypes
+lang_name="Python Scripting"
+print(lang_name)
+print(type(lang_name))
+
+my_name=('arunputhal')
+print(my_name)
+print(type(my_name))
+
+# Boolean Datatypes
+my_value=True
+my_new_value=False
+print(my_value, type(my_value))
+print(my_new_value, type(my_new_value))
+
+# Typecasting or type conversion datatypes
+x=56
+print(x, type(x))
+y=str(x)
+print(y, type(y))
+z=bool(x)
+print(z, type(z))
+p=0
+print(p, type(p))
+q=bool(p)
+print(q, type(q))
