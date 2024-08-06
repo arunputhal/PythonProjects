74e45bd8a4b5b9323764256be3963195b4187ee3
arunputhal <arunputhal@gmail.com>
2024-08-07 00:33:19 +0530

    python variable

---
+++ input_output_script.py
@@ -0,0 +1,7 @@
+# Sample Arithmetic Calculator
+a=eval(input("Enter the value for a:"))
+b=eval(input("Enter the value for b:"))
+print(f'The value of "a" is {a} and type of "a" is: {type(a)}')
+print(f'The value of "b" is {b} and type of "b" is {type(b)}')
+result=a+b
+print(f'The addition of {a} and {b} is {result}')
---
+++ multiple_variable_in_print.py
@@ -0,0 +1,11 @@
+x=3
+y=5.7
+lang_name="python"
+
+# print(x,y,lang_name)
+# print("{} \n{} \n{}".format(x,y,lang_name))
+# print("{} {} {}".format(y,x,lang_name))
+# print(f"values are: {x} {y} {lang_name}")
+# print(f"x value is {x} \ny value is {y} \nLanguage is {lang_name}")
+my_req_value=f"x value is {x} \ny value is {y} \nLanguage is {lang_name}"
+print(my_req_value)
